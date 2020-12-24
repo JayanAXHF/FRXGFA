70d0a1b0dc0fd676e85991e5f5e5a9cb86482d3a
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2020-12-24 21:01:13 +0530

    Update X86JS.js

--- X86JS.js
+++ X86JS.js
@@ -5,10 +5,7 @@ ctx = canvas.getContext("2d");
 
 rover_width = 100;
 rover_height = 90;
-images = "https://i.pinimg.com/originals/d0/9c/4c/d09c4cfd1b93cc66dc13c8b461fd2145.jpg";
-background_image = images;
 rover_image = "https://i.postimg.cc/YqdnnNX1/car1.png";
-image2 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvl3nVz5OV6ziClv6dufNHDFYi3OuBnQnI0w&usqp=CAU         "
 rover_x = 10;
 rover_y = 10;
 rover_y2 = 100;
@@ -31,6 +28,10 @@ function add() {
 
 }
 
+
+function uploadBackground() {
+    ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
 
 function uploadRover() {
     ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
@@ -83,5 +84,31 @@ function keydown1(e) {
             uploadRover();
 
         }
+    }
+    if (e.shiftKey == true && keyPressed == "70") {
+        rover_width = rover_width + 10;
+        console.log("F ~ file: X86JS.js ~ line 92 ~ keydown1 ~ rover_width", rover_width);
+        rover_height = rover_height + 10;
+        console.log("F ~ file: X86JS.js ~ line 94 ~ keydown1 ~ rover_height", rover_height);
+        localStorage.setItem("shift + f", rover_height + rover_width)
+    }
+
+    if (e.shiftKey == true && keyPressed == "66") {
+        rover_width = rover_width - 10;
+        console.log("F ~ file: X86JS.js ~ line 92 ~ keydown1 ~ rover_width", rover_width);
+        rover_height = rover_height - 10;
+        console.log("F ~ file: X86JS.js ~ line 94 ~ keydown1 ~ rover_height", rover_height);
+        localStorage.setItem("shift + b", rover_height + rover_width)
+
+    }
+
+
+    if (keyPressed = "83") {
+        newImage("https://lh6.googleusercontent.com/Gm4ExrJQ0HMD4DJ35On6ptiqT_nV4MNQZvMe8LwsRjO1dBLtsV3KLYD4a8Ef_ksLFocNWJLgMHrySdbk8taM=w1280-h665") uploadRover();
+
+    }
+
+    if (keyPressed = "72") {
+        newImage("https://lh5.googleusercontent.com/VkpN1K9H700oOKEipeewRYkzqa0awRLHRPsqFKuSBQxp024DlFGKuR7-G5Q4OVsIxJNAp3Zf2VKcT5WtLiFE=w1280-h665")
     }
 }
