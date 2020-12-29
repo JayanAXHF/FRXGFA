c9cfb4fb2296e1f79298dd3924cf397bc57aaeee
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2020-12-29 23:37:26 +0530

    Add files via upload

---
+++ X89JS.js
@@ -0,0 +1,6 @@
+function addUser() {
+    localStorage.setItem("p1n", document.getElementById("player1_name_input").value);
+    localStorage.setItem("p2n", document.getElementById("player2_name_input").value)
+
+    location = "X89.html";
+}
