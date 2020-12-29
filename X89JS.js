b5e5f9ba021fbea9975dac7d7ed4207032d45ea5
JayanAXHF <73993003+JayanAXHF@users.noreply.github.com>
2020-12-29 23:44:26 +0530

    Update X89JS.js

--- X89JS.js
+++ X89JS.js
@@ -2,5 +2,5 @@ function addUser() {
     localStorage.setItem("p1n", document.getElementById("player1_name_input").value);
     localStorage.setItem("p2n", document.getElementById("player2_name_input").value)
 
-    location = "X89.html";
+    location = "X89B.html";
 }
